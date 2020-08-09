0fd59345bea2acf4fa0f87d71a47d3aa6565fb8a
collierrgbsitisfise <vadim.vicolaev.v@gmail.com>
2020-08-09 22:27:25 +0300

    get bidirectional linked articles

--- index.ts
+++ index.ts
@@ -1,15 +1,19 @@
 import { WikiParse } from './services/wikiParse';
 import { TextAnalizer } from './services/textAnalizer';
 
+const start = '/wiki/United_States';
 
 (async () => {
   const {
     text,
     html,
-  } = await WikiParse.getWikiContent('https://en.wikipedia.org/wiki/United_States');
+  } = await WikiParse.getWikiContent(start);
 
   const urls = WikiParse.getAllInternalUrls(html);
 
-  const textAnalizer = new TextAnalizer();
-  console.log(textAnalizer.getWordsValues(text));
+  console.log('urls : ', urls);
+  const n = await WikiParse.getBiDirectionalLinkedArticle(start, urls);
+  console.log(n.length);
+  // const textAnalizer = new TextAnalizer();
+  // console.log(textAnalizer.getWordsValues(text));
 })();
--- services/wikiParse.ts
+++ services/wikiParse.ts
@@ -2,8 +2,10 @@ import * as cheerio from "cheerio";
 import got from 'got';
 
 export class WikiParse {
+  static basePath: string = 'https://en.wikipedia.org';
   static async getWikiContent(url: string): Promise<WikiPageResponse> {
-    const { body } = await got(url);
+    console.log('GET : ', `${this.basePath}${url}`);
+    const { body } = await got(`${this.basePath}${url}`);
     const $ = cheerio.load(body);
     return {
       html: $("#bodyContent").html(),
@@ -22,11 +24,47 @@ export class WikiParse {
     });
 
     const isInternalLink = url => typeof url === 'string' && url.split('/')[1] === 'wiki';
-    return result.filter(isInternalLink);
+    return result.filter(isInternalLink).map(link => link.toLowerCase());
+  }
+
+  static async getBiDirectionalLinkedArticle(
+    mainLink: string,
+    links: string[],
+    limit: number = 10,
+  ): Promise<BidirectioanlLinkedArticle[]> {
+    const res: BidirectioanlLinkedArticle[] = [];
+
+    for (const link of links) {
+      try {
+        const { text, html } = await WikiParse.getWikiContent(link);
+        const articleLinks = WikiParse.getAllInternalUrls(html);
+        console.log('articleLinks : ', articleLinks);
+        if (articleLinks.includes(mainLink.toLowerCase())) {
+          console.log('Lucky : ', link);
+          res.push({
+            text,
+            url: link,
+          });
+        }
+      } catch(err) {
+        continue;
+      }
+
+      if (res.length === limit) {
+        return res;
+      }
+    }
+
+    return res;
   }
 }
 
 export type WikiPageResponse = {
   html: string;
+  text: string;
+};
+
+export type BidirectioanlLinkedArticle = {
+  url: string;
   text: string;
 };
